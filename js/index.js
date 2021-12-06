ec2eaa0339c8b79d5a026c5e350898320ae0e8cc
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-06 10:35:45 +0900

    [Feature] - Display navigation bar has completed

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 const uls = document.querySelectorAll("nav ul");
 const links = [...document.querySelectorAll("nav a")];
-const light = document.querySelector("nav .tubelight");
+const light = document.querySelector("nav .tube-light");
+console.log(light);
+// document.querySelectByClassname()
 
 let activeIndex = 0;
 let cursorIndex = 0;
 let increment = 1;
+
 links.forEach((link, index) => {
-    if (links[index].classList.containes("active")) {
+    if (links[index].classList.contains("active")) {
         light.style.left= `${links[index].offsetLeft + light.offset.Width / 4}px`;
     }
 
@@ -19,6 +22,20 @@ links.forEach((link, index) => {
         let t = setInterval(() => {
             if (activeIndex > cursorIndex) increment = 1;
             else if (activeIndex < cursorIndex) increment = -1;
-        });
+            
+            cursorIndex += increment;
+            links[cursorIndex].classList.add("active");
+
+            if(cursorIndex != -1)
+            links[cursorIndex -increment].classList.remove("active");
+
+            if(cursorIndex == activeIndex) {
+                e.target.classList.add("active");
+                increment = 0;
+                clearInterval(t);
+            }
+        }, 50);
+
+        light.style.left = `${e.target.offsetLeft + light.offset.Width / 4}px`;
     });
 });
