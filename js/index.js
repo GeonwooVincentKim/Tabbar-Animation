316de8e8960d092145866b6cfcc1a01ac64dbb86
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-06 10:06:57 +0900

    [Feature] - Applying Links for each element of `i` tags

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+const uls = document.querySelectorAll("nav ul");
+const links = [...document.querySelectorAll("nav a")];
+const light = document.querySelector("nav .tubelight");
+
+let activeIndex = 0;
+let cursorIndex = 0;
+let increment = 1;
+links.forEach((link, index) => {
+    if (links[index].classList.containes("active")) {
+        light.style.left= `${links[index].offsetLeft + light.offset.Width / 4}px`;
+    }
+
+    link.addEventListener("click", (e) => {
+        activeIndex = index;
+        console.log(activeIndex);
+
+        let t = setInterval(() => {
+            if (activeIndex > cursorIndex) increment = 1;
+            else if (activeIndex < cursorIndex) increment = -1;
+        });
+    });
+});
